a392862f278dad0269b2ecea0cdff53d5d951c15
David Nieto <u2903149@unimilitar.edu.co>
2023-06-12 10:35:54 -0500

    Redes-sociales

--- src/componentes/Footer/index.jsx
+++ src/componentes/Footer/index.jsx
@@ -3,14 +3,17 @@ import "./Footer.css"
 const Footer = () => {
     return <footer className='footer' style={{ backgroundImage: "url(/img/footer.png)" }}>
         <div className='redes'>
-            <a href='https://www.aluracursos.com/'>
+            <a href='https://www.facebook.com/davichechenco/'>
                 <img src="/img/facebook.png" alt='Facebook' />
             </a>
-            <a href='https://www.aluracursos.com/'>
-                <img src="/img/twitter.png" alt='twitter' />
+            <a href='https://github.com/David-F-98'>
+                <img src="/img/github.png" alt='github' />
             </a>
-            <a href='https://www.aluracursos.com/'>
+            <a href='https://instagram.com/davichechenco?igshid=MzNlNGNkZWQ4Mg=='>
                 <img src="/img/instagram.png" alt='instagram' />
+            </a>
+            <a href='https://www.linkedin.com/in/david-rodr%C3%ADguez-freelancerprogramacion/'>
+                <img src="/img/linkedin.png" alt='linkedin' />
             </a>
         </div>
         <img src='/img/Logo.png' alt='org' />
